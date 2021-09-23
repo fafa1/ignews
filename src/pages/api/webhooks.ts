f467c06671f32dfc6379ee18263aaea8e837f28c
fagner batista <fagnerbatista@MacBook-Air-de-fagner.local>
2021-09-22 23:42:44 -0300

    penultima aula

--- src/pages/api/webhooks.ts
+++ src/pages/api/webhooks.ts
@@ -2,6 +2,7 @@ import { NextApiRequest, NextApiResponse } from "next";
 import { Readable } from "stream";
 import Stripe from "stripe";
 import { stripe } from "../../services/stripe"
+import { saveSubscription } from "./_lib/manageSubscription";
 
 async function buffer(readble: Readable) {
     const chunks = [];
@@ -42,10 +43,24 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
         const { type } = event;
 
         if (relevantEvents.has(type)) {
-            console.log('evento recebido', event)
-            
-        }
+					try {
+						switch(type) {
+							case 'checkout.session.completed':
 
+								const checkoutSession = event.data.object as Stripe.Checkout.Session
+
+								await saveSubscription(
+									checkoutSession.subscription.toString(),
+									checkoutSession.customer.toString()
+								)
+								break
+							default:
+								throw new Error('Unhandled event.')
+						}
+					} catch (error) {
+							return res.json({error: 'webhook faield'})							
+					}
+        }
         res.json({ received: true })
     } else {
         res.setHeader('Allow', 'POST')
